fb6683985485e5f856796f2326c484e78b98f96b
Jinzhiqi420 <90999026+Jinzhiqi420@users.noreply.github.com>
2023-03-02 04:08:30 -0500

    unittest
    add unit test

--- app/src/test/java/com/example/bogglebar/ExampleUnitTest.kt
+++ app/src/test/java/com/example/bogglebar/ExampleUnitTest.kt
@@ -1,24 +1,26 @@
 package com.example.bogglebar
 
-import android.widget.TextView
-import androidx.test.core.app.ApplicationProvider
-import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Assert.*
 import org.junit.Test
 
-import org.junit.Assert.*
-import org.junit.Before
-import org.junit.runner.RunWith
 
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
 class ExampleUnitTest {
+
     @Test
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
     }
+    @Test
+    fun boggleFragment_initializationTest() {
+        val boggleFragment = BoggleFragment()
+        assertNotNull(boggleFragment)
+    }
+
+    @Test
+    fun scoreFragment_initializationTest() {
+        val scoreFragment = ScoreFragment()
+        assertNotNull(scoreFragment)
+    }
+
+
 }
-
-
